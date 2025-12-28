8e997a8827f1ee92517c2ff922da71d094bf296f
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-12-28 18:44:31 +0530

    updated the install.js

---
+++ src/commands/install.js
@@ -0,0 +1,26 @@
+import fs from "fs"; 
+import path from "path"; 
+import { getPackagesDir } from "../core/path.js";
+import { getRepoUrl } from "../core/github.js"; 
+import { run } from "../utils/exec.js";
+
+export async function install(pkgName) {
+  const baseDir = getPackagesDir(); 
+  const targetDir = path.join(baseDir , pkgName);
+
+  if (!fs.existsSync(baseDir)){
+    fs.mkdirSync(baseDir);
+  }
+
+  if (fs.existsSync(targetDir)) {
+    fs.mkdirSync(baseDir);
+  }
+
+  if (fs.existsSync(targetDir)) {
+    throw new Error(`Package '${pkgName}' already installed`);
+  }
+
+  const repoUrl = getRepoUrl(pkgName); 
+  
+  await run("git" , ["clone" , repoUrl , targetDir]);
+}
