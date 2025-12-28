99cfdcc7239f11f9f00e6e394b26f96b34f6fa7d
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-12-28 18:51:37 +0530

    updated

---
+++ bin/v.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { install } from "../src/commands/install.js";
+
+const [, , command , arg] = process.argv; 
+
+if (!command) {
+  console.error("Usage: v <command> [args]");
+  process.exit(1); 
+}
+
+try{
+  if(command === "install") {
+    if (!arg) {
+      throw new Error("Package name required"); 
+    }
+    await install(arg);
+  } else {
+    throw new Error(`Unknown command: ${command}`);
+  }
+} catch (err) {
+  console.error(err.message);
+  process.exit(1);
+}
+
